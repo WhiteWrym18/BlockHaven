8db08850d32a178bb3d25fc0da08d6410b0352eb
WhiteWrym18 <stefano165@outlook.it>
2025-05-11 14:30:57 +0200

    Defined get_block and set_block in grid_utils.py

--- src/world.py
+++ src/world.py
@@ -1,23 +1,22 @@
 from typing import List, Optional
 from blocktype import BlockType  # Import BlockType from blocktype.py
 
-class World: 
-   def __init__(self, width: int, height: int):
-      self.width = width
-      self.height = height
-      self.grid: List [List[Optional[BlockType]]] = [] # 2D grid of Blocktype 
-      self.generate_world()
+class World:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.grid: List[List[Optional[BlockType]]] = []  # 2D grid of BlockType
+        self.generate_world()
 
     def generate_world(self):
-        "Generate the world with blocks."
+        """Generate the world with blocks."""
         for y in range(self.height):
             row = []
             for x in range(self.width):
                 if y < self.height - 5:
-                    row.append(BlockType.GRASS) # Top Layers are grass 
+                    row.append(BlockType.GRASS)  # Top layers are grass
                 elif y < self.height - 1:
-                    row.append(BlockType.DIRT)  # Middle Layers are dirt 
-                else: 
-                    row.append(BlockType.STONE) # Bottom layers are stone
+                    row.append(BlockType.DIRT)  # Middle layers are dirt
+                else:
+                    row.append(BlockType.STONE)  # Bottom layers are stone
             self.grid.append(row)
-
