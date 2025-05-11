b9beed3515a152d7e14b8e93230e3ce853c0ccee
WhiteWrym18 <stefano165@outlook.it>
2025-05-11 17:15:51 +0200

    Completed Rewrite

--- src/main.py
+++ src/main.py
@@ -4,3 +4,16 @@ from blocktype import BlockType
 from turtle_render import setup_renderer, render_world, render_player
 from keybindings import setup_keybindings
 
+def main():
+    world = World(20,15)
+    player = Player(10,7,world)
+    screen = setup_renderer()
+
+    setup_keybindings(screen, player)
+
+    while True:
+        render_world(world)
+        render_player(player)
+        screen.update()
+if __name__ == "__main__":
+    main()
