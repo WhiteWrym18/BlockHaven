dfa317374f0e108936451d0e5f2af95b2074880a
WhiteWrym18 <stefano165@outlook.it>
2025-05-11 16:09:12 +0200

    Set up Turtle Rendering

--- src/turtle_render.py
+++ src/turtle_render.py
@@ -3,3 +3,10 @@ from world import World
 from Player import Player 
 from blocktype import BlockType
 
+def setup_render():               # Set Up Turtle Rendering 
+    screen = turtle.Screen()
+    screen.title("BlockHaven - A Simple Minecraft Clone")
+    screen.setup(width=800, height=600)
+    screen.tracer(0)
+    return screen
+
