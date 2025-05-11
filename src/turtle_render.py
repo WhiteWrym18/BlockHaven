cbc92a1b44d0856e091109a2324acd7af6b1c7d3
WhiteWrym18 <stefano165@outlook.it>
2025-05-11 16:19:52 +0200

    Defined render_world in turltle_render.py

--- src/turtle_render.py
+++ src/turtle_render.py
@@ -10,3 +10,23 @@ def setup_render():               # Set Up Turtle Rendering
     screen.tracer(0)
     return screen
 
+def render_world(world: World):
+    turtle.clear()
+    for y, row in enumerate(world.grid):
+        for x, block in enumerate(row):
+            if block:
+                turtle.penup
+                turtle.goto(x*20 - 200, 200 - y *20)
+                turtle.pendown()
+                if block == BlockType.GRASS:
+                    turtle.color("green")
+                elif block == BlockType.DIRT:
+                    turtle.color("brown")
+                elif block == BlockType.STONE:
+                    turtle.color("gray")
+                turtle.begin_fill()
+                for _ in range(4):
+                    turtle.forward(20)
+                    turtle.right(90)
+                turtle.end_fill()
+    turtle.update()
