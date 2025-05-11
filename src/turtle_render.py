85d7b476f95bc159f5af8199d3c0682df4a0c181
WhiteWrym18 <stefano165@outlook.it>
2025-05-11 16:25:55 +0200

    Complete turtle render

--- src/turtle_render.py
+++ src/turtle_render.py
@@ -10,7 +10,7 @@ def setup_render():               # Set Up Turtle Rendering
     screen.tracer(0)
     return screen
 
-def render_world(world: World):
+def render_world(world: World):       # Render the world grid using turtle.
     turtle.clear()
     for y, row in enumerate(world.grid):
         for x, block in enumerate(row):
@@ -29,4 +29,12 @@ def render_world(world: World):
                     turtle.forward(20)
                     turtle.right(90)
                 turtle.end_fill()
+    turtle.update()
+
+def render_player(player: Player):      # Render the player on the grid.
+    turtle.penup()
+    turtle.goto(player.x*20 - 200, 200 - player.y*20)
+    turtle.shape("circle")
+    turtle.color("blue")
+    turtle.stamp()
     turtle.update()
