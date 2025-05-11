fb899584be6de352b066706c6dacc9207fd8ee11
WhiteWrym18 <stefano165@outlook.it>
2025-05-11 15:26:07 +0200

    Completed rewrite of player.ts

--- src/player.py
+++ src/player.py
@@ -1,3 +1,6 @@
+from blocktype import BlockType
+from world import World
+
 class Player:
     def __init__(self, x: int, y: init):
         """:param x: Initial x-cordinate of the player.
@@ -18,5 +21,11 @@ class Player:
     def move_right(self, step: init = 1): # Move the player right by a specified step size.
         self.x += step
     
+    def place_block(self, block_type: BlockType):   # Place a block at the player's current position.
+        self.world.set_block(self.x, self.y, block_type)
+    
+    def remove_block(self):      # Remove a block at the player's current position.
+        self.world.set_block(self.x, self.y, None)
+
     def get_position(self):               # Return the current position of the player as a tuple.
         return self.x, self.y
